9b0bddbc7eb035b21c77728d00b8f3696a7213b9
Habyb Fernandes <habybfernandes@gmail.com>
2021-03-29 08:53:23 -0300

    updated baseUrl

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -8,4 +8,4 @@ final Client client = HttpClientWithInterceptor.build(
   requestTimeout: Duration(seconds: 5),
 );
 
-const String baseUrl = 'https://bcd68e426002.ngrok.io/transactions';
+const String baseUrl = 'https://64ac9cc59e7e.ngrok.io/transactions';
